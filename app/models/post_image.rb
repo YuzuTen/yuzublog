cdc9ecc88c2f2d47cbd96e69ce4f14ea7408cff9
JasonTrue <none@none>
2011-03-09 16:51:57 -0800

    fixed thumbnail and resize styles

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,7 @@
 class PostImage < ActiveRecord::Base
   belongs_to :post
   has_attached_file :photo, :storage=> :s3, :s3_credentials => {  :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] },
-  :bucket => 'me.yuzuten.com',  :s3_host_alias => 'me.yuzuten.com', :url => ':s3_alias_url', :path => 'post_images/:attachment/:id/:style/:basename.:extension',:s3_styles => { :thumb => '64x64#', :small => '150x150>', :medium => '320x240>', :large => '640x480>' }
+  :bucket => 'me.yuzuten.com',  :s3_host_alias => 'me.yuzuten.com', :url => ':s3_alias_url', :path => 'post_images/:attachment/:id/:style/:basename.:extension',:styles => { :thumb => '64x64#', :small => '150x150>', :medium => '320x240>', :large => '640x480>' }
   validates_attachment_presence :photo
   validates_attachment_size :photo
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/pjpeg', 
